03382ab16061f1091410bbd31631891051f6ba4f
Don Welch <dwelch@nait.ca>
2020-09-18 08:41:54 -0600

    Review Wall class, cover Composite class Room

---
+++ OOPsReview/ConsoleApp/Room.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+   
+    public class Room
+    {
+        //composite class
+        //is a mixture of data members/properties
+        //   of native datatypes and other classes
+
+        private string _Name;
+        public string Name
+        {
+            get { return _Name; }
+            set { _Name = string.IsNullOrEmpty(value) ? "Unknown" : value; }
+        }
+    
+        public List<Wall> Walls { get; set; }
+        public List<Window> Windows { get; set; }
+        public List<Door> Doors { get; set; }
+
+        public Room()
+        {
+            Name = "Unknown";
+        }
+    }
+    
+}
--- OOPsReview/ConsoleApp/Wall.cs
+++ OOPsReview/ConsoleApp/Wall.cs
@@ -6,12 +6,73 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp
 {
-    class Wall
+    public class Wall
     {
         // height, width
         //height and width > 0.0
         //height has a defualt of 2.5, width is 4.25
         //Area 
         //throw exceptions for invalid data
+
+        private decimal _Height;
+        private decimal _Width;
+
+        public decimal Height
+        {
+            get
+            {
+                return _Height;
+            }
+            set
+            {
+                // the m indicates the value is a decimal
+                if (value <= 0.0m)
+                {
+                    throw new Exception("Height can not be 0 or less than 0.");
+                }
+                else
+                {
+                    _Height = value;
+                }
+            }
+        }
+
+        public decimal Width
+        {
+            get
+            {
+                return _Width;
+            }
+            set
+            {
+                if (value <= 0.0m)
+                {
+                    throw new Exception("Width can not be 0 or less than 0.");
+                }
+                else
+                {
+                    _Width = value;
+                }
+            }
+        }
+
+        public Wall()
+        {
+            //instead of using the system datatype defaults, we have supplied
+            //  our own defaults
+            Width = 4.25m;
+            Height = 2.5m;
+        }
+
+        public Wall(decimal width, decimal height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public decimal WallArea()
+        {
+            return Width * Height;
+        }
     }
 }
