04ea6ea721c269430412d9bf183428a73f830d63
Don Welch <dwelch@nait.ca>
2020-11-20 08:58:40 -0700

    Complete simple query Region lookup, fix error in WebConnectionStrings

---
+++ CSNet/WebApp/SamplePages/SimpleQuery.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Title="Simple Query" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="SimpleQuery.aspx.cs" Inherits="WebApp.SamplePages.SimpleQuery" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="row">
+        <div class ="jumbotron">
+            <h1>Simple Query</h1>
+            <blockquote class="alert alert-info">This page will input a region number and display
+                the record record matching the input value.
+            </blockquote>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-6">
+            <asp:Label ID="Label1" runat="server" 
+                Text="Enter a region id:"></asp:Label>
+            &nbsp;&nbsp;
+            <asp:TextBox ID="RegionArg" runat="server"></asp:TextBox>
+            &nbsp;&nbsp;
+            <asp:Button ID="RegionSearch" runat="server" Text="Region ?" OnClick="RegionSearch_Click" />
+        </div>
+        <div class="col-6">
+            <asp:Label ID="Label2" runat="server" 
+                Text="Region:"></asp:Label>
+            &nbsp;&nbsp;
+            <asp:Label ID="RegionId" runat="server" ></asp:Label>
+            <br /><br />
+             <asp:Label ID="Label4" runat="server" 
+                 Text="Description"></asp:Label>
+            &nbsp;&nbsp;
+            <asp:Label ID="RegionDescription" runat="server" ></asp:Label>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-12 text-center">
+            <asp:Label ID="MessageLabel" runat="server" ></asp:Label>
+        </div>
+    </div>
+</asp:Content>
---
+++ CSNet/WebApp/SamplePages/SimpleQuery.aspx.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Drawing;
+
+#region Additional Namespaces
+using NorthwindSystem.BLL;
+using NorthwindSystem.Entities;
+#endregion
+
+namespace WebApp.SamplePages
+{
+    public partial class SimpleQuery : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //empty out old messages
+            MessageLabel.Text = "";
+        }
+
+        protected void RegionSearch_Click(object sender, EventArgs e)
+        {
+            //verify input exists
+            if (string.IsNullOrEmpty(RegionArg.Text))
+            {
+                MessageLabel.Text = "Enter a region id";
+            }
+            else
+            {
+                //you could do other validations such as: numeric check,
+                //   range check (>0), ...
+
+                //standard look up
+                try
+                {
+                    //connect to controller class by creating an instance
+                    RegionController sysmgr = new RegionController();
+
+                    //issue your call to the class instance
+                    NorthwindSystem.Entities.Region info = sysmgr.Region_FindByID(int.Parse(RegionArg.Text));
+
+                    //test results and either display record or an approriate message
+                    if (info == null)
+                    {
+                        //no found
+                        MessageLabel.Text = "No region for supplied value";
+                        RegionId.Text = "";
+                        RegionDescription.Text = "";
+                    }
+                    else
+                    {
+                        //RegionId is a point to the label itself
+                        //.Text is a property of the Label instance which
+                        //     allow one to change its contents.
+                        RegionId.Text = info.RegionID.ToString();
+                        RegionDescription.Text = info.RegionDescription;
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    MessageLabel.Text = $"Error: {ex.Message}";
+                }
+            }
+        }
+    }
+}
